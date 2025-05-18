14e81cf0c1619deee36ef317fb4154b2ed6b33ce
Artur Smýkal <1133@student.itgymnazium.cz>
2025-05-18 18:08:31 +0200

    c1

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+<header>
+  <img src="../assets/images/heart.png" alt="heart">
+  <h1 class="rainbow-text">
+    LOVE IS LOVE
+  </h1>
+  <h2>
+    Discover the beauty of romance
+  </h2>
+</header>
+</template>
+
+<style scoped>
+
+header {
+  height:30rem;
+  width: 75rem;
+  border-bottom-right-radius: 36rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: start;
+  padding: 3rem;
+  background: var(--vt-c-black-soft);
+}
+
+img {
+  width: 20rem;
+  aspect-ratio: 1 / 1;
+}
+
+</style>
